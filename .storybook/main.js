fd84159f513898ff3ec6ec0dd499d733881820a9
tkshio <tkshio>
2022-03-30 07:16:38 +0000

    StoryBookを更新（react-refreshの指定はReact18リリースに伴う暫定対処）

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
+
+
 module.exports = {
-    "stories": [
+    stories: [
         "../src/**/*.stories.mdx",
         "../src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
-    "addons": [
+    addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/preset-create-react-app"
-    ]
+    ],
+    staticDirs: ['../public'],
+    core: { "builder": 'webpack5' },
 }
