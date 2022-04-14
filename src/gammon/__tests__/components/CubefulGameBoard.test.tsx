786e7e8038ca9ad5416d274e1dba3fd4341cdd03
tkshio <tkshio>
2022-04-14 13:54:54 +0000

    CubefulGameBoardのテストの各処理を整理、プレイ過程を追加

--- src/gammon/__tests__/components/CubefulGameBoard.test.tsx
+++ src/gammon/__tests__/components/CubefulGameBoard.test.tsx
@@ -3,23 +3,35 @@ import { ComponentProps } from 'react'
 import { render, screen } from '@testing-library/react'
 import { unmountComponentAtNode } from 'react-dom'
 import {
+    CBOperator,
     CubefulGameBoard,
     CubefulGameConfs,
 } from '../../components/CubefulGameBoard'
 import { toCBState, toSGState } from '../../dispatchers/utils/GameState'
-import { CubeGameListeners } from '../../dispatchers/CubeGameDispatcher'
-import { SingleGameListeners } from '../../dispatchers/SingleGameDispatcher'
+import {
+    CubeGameListeners,
+    setStateListener as setCBStateListener,
+} from '../../dispatchers/CubeGameDispatcher'
+import {
+    SingleGameListeners,
+    setStateListener as setSGStateListener,
+} from '../../dispatchers/SingleGameDispatcher'
 import { SGState } from '../../dispatchers/SingleGameState'
 import { CBState } from '../../dispatchers/CubeGameState'
 import { presetDiceSource } from 'tsgammon-core/utils/DiceSource'
-import { CheckerPlayListeners } from '../../dispatchers/CheckerPlayDispatcher'
+import {
+    CheckerPlayListeners,
+    setStateListener as setCPStateListener,
+} from '../../dispatchers/CheckerPlayDispatcher'
 import { CheckerPlayState } from '../../dispatchers/CheckerPlayState'
-import { SingleGameConfs } from '../../components/SingleGameBoard'
 import {
     redCBAutoOperator,
     redSGAutoOperator,
+    whiteCBAutoOperator,
+    whiteSGAutoOperator,
 } from '../../dispatchers/autoOperators'
 import { act } from 'react-dom/test-utils'
+import { SGOperator } from '../../components/SingleGameBoard'
 
 let container: HTMLElement | null = null
 
@@ -29,62 +41,15 @@ beforeEach(() => {
 
     jest.useFakeTimers()
 })
-const state: {
-    cpState?: CheckerPlayState
-    sgState: SGState
-    cbState: CBState
-} = {
+
+const state = {
     cpState: undefined,
     sgState: toSGState(),
     cbState: toCBState(),
 }
-function setCPState(next: CheckerPlayState) {
-    state.cpState = next
-}
-function setSGState(next: SGState) {
-    state.sgState = next
-}
-function setCBState(next: CBState) {
-    state.cbState = next
-}
 
-const cpListeners: CheckerPlayListeners = {
-    onCheckerPlay: setCPState,
-    onCommitCheckerPlay: () => {
-        //
-    },
-    onUndo: setCPState,
-    onRedo: setCPState,
-    onRevertDices: setCPState,
-}
-const sgListeners: SingleGameListeners = {
-    onStartOpeningCheckerPlay: setSGState,
-    onRerollOpening: setSGState,
-    onAwaitRoll: setSGState,
-    onStartCheckerPlay: setSGState,
-    onEndOfGame: setSGState,
-}
-const cbListeners: CubeGameListeners = {
-    onStartCubeAction: setCBState,
-    onAwaitCheckerPlay: setCBState,
-    onDouble: setCBState,
-    onTake: setCBState,
-    onSkipCubeAction: setCBState,
-    onEndOfCubeGame: setCBState,
-}
+const props = setupListeners(state)
 
-const diceSource = presetDiceSource(1, 3, 4, 5)
-const props: ComponentProps<typeof CubefulGameBoard> = {
-    ...state,
-    ...cbListeners,
-    ...sgListeners,
-    ...cpListeners,
-    cbConfs: {
-        sgConfs: {
-            diceSource,
-        },
-    },
-}
 describe('CubeGameBoard', () => {
     test('does opening roll when dice gets clicked', async () => {
         render(<CubefulGameBoard {...props} />)
@@ -95,16 +60,11 @@ describe('CubeGameBoard', () => {
         dices.forEach((dice) => expect(dice).toBeInTheDocument())
         await userEvent.click(dices[0])
         expect(state.sgState.tag).toEqual('SGInPlay')
+        expect(isWhite(state.sgState)).toBeTruthy()
     })
+
     test('moves a piece when point 19 gets clicked', async () => {
-        const onCheckerPlay = jest.fn((cp) =>
-            (
-                props.onCheckerPlay ??
-                ((_: CheckerPlayState) => {
-                    //
-                })
-            )(cp)
-        )
+        const onCheckerPlay = jest.fn(props.onCheckerPlay)
         const next = { ...props, ...state, onCheckerPlay }
         render(<CubefulGameBoard {...next} />)
 
@@ -112,15 +72,9 @@ describe('CubeGameBoard', () => {
         userEvent.click(point)
         expect(onCheckerPlay).toBeCalled()
     })
+
     test('moves a piece when point 17 gets clicked', async () => {
-        const onCheckerPlay = jest.fn((cp) =>
-            (
-                props.onCheckerPlay ??
-                ((_: CheckerPlayState) => {
-                    //
-                })
-            )(cp)
-        )
+        const onCheckerPlay = jest.fn(props.onCheckerPlay)
         const next = { ...props, ...state, onCheckerPlay }
         render(<CubefulGameBoard {...next} />)
 
@@ -128,6 +82,7 @@ describe('CubeGameBoard', () => {
         userEvent.click(point)
         expect(onCheckerPlay).toBeCalled()
     })
+
     test("commits one's ply when dice gets clicked", async () => {
         const next = { ...props, ...state }
         render(<CubefulGameBoard {...next} />)
@@ -135,56 +90,178 @@ describe('CubeGameBoard', () => {
         const whiteDice = screen.getByTestId(/^dice-right/)
         userEvent.click(whiteDice)
         expect(state.sgState.tag).toEqual('SGToRoll')
-        expect(
-            state.sgState.tag === 'SGToRoll' ? state.sgState.isRed : false
-        ).toBeTruthy()
         expect(state.cbState.tag).toEqual('CBAction')
+        expect(isRed(state.sgState)).toBeTruthy()
     })
-    test("lets redAutoPlayer do red's ply", async () => {
-        const onSkipCubeAction = jest.fn((cb) =>
-            (
-                props.onSkipCubeAction ??
-                ((_: CheckerPlayState) => {
-                    //
-                })
-            )(cb)
-        )
-        const sgConfs: SingleGameConfs = {
-            ...props.cbConfs?.sgConfs,
-            autoOperator: redSGAutoOperator(),
-        }
-        const rao = redCBAutoOperator()
-        const ao = {
-            ...rao,
-            operateRedCubeAction: jest.fn(rao.operateRedCubeAction),
-        }
-        const cbConfs: CubefulGameConfs = {
-            ...props.cbConfs,
-            sgConfs,
-            autoOperator: ao,
+
+    test("lets redAutoPlayer do red's cubeAction", async () => {
+        const onSkipCubeAction = jest.fn(props.onSkipCubeAction)
+        const cbConfs = setRedAutoOp(props)
+        const next = {
+            ...props,
+            ...state,
+            onSkipCubeAction,
+            cbConfs: {
+                ...cbConfs,
+                autoOperator: cbConfs.autoOperator
+                    ? {
+                          ...cbConfs.autoOperator,
+                          operateRedCubeAction: jest.fn(
+                              cbConfs.autoOperator?.operateRedCubeAction
+                          ),
+                      }
+                    : undefined,
+            },
         }
 
-        const next = { ...props, ...state, onSkipCubeAction, cbConfs }
         render(<CubefulGameBoard {...next} />)
         act(() => {
             jest.advanceTimersByTime(10)
         })
-        expect(ao.operateRedCubeAction).toBeCalled()
+        expect(next.cbConfs?.autoOperator?.operateRedCubeAction).toBeCalled()
         expect(onSkipCubeAction).toBeCalled()
         expect(state.cbState.tag).toEqual('CBToRoll')
         expect(state.sgState.tag).toEqual('SGToRoll')
-        expect(
-            state.sgState.tag === 'SGToRoll' ? state.sgState.isRed : false
-        ).toBeTruthy()
+        expect(isRed(state.sgState)).toBeTruthy()
+    })
+
+    test("lets redAutoPlayer do red's checkerPlay", async () => {
+        const next = { ...props, ...state, cbConfs: setRedAutoOp(props) }
+        render(<CubefulGameBoard {...next} />)
+        act(() => {
+            jest.advanceTimersByTime(10)
+        })
+        expect(state.cbState.tag).toEqual('CBInPlay')
+        expect(state.sgState.tag).toEqual('SGInPlay')
+        expect(isRed(state.sgState)).toBeTruthy()
+    })
+
+    test("lets redAutoPlayer do red's commit", async () => {
+        const next = { ...props, ...state, cbConfs: setRedAutoOp(props) }
+        render(<CubefulGameBoard {...next} />)
+        act(() => {
+            jest.advanceTimersByTime(10)
+        })
+        expect(state.cbState.tag).toEqual('CBAction')
+        expect(state.sgState.tag).toEqual('SGToRoll')
+        expect(isWhite(state.sgState)).toBeTruthy()
+    })
+
+    test("lets whiteAutoPlayer do white's cubeAction", async () => {
+        const onSkipCubeAction = jest.fn(props.onSkipCubeAction)
+        const cbConfs = setWhiteAutoOp(props)
+        const next = {
+            ...props,
+            ...state,
+            onSkipCubeAction,
+            cbConfs: {
+                ...cbConfs,
+                autoOperator: cbConfs.autoOperator
+                    ? {
+                          ...cbConfs.autoOperator,
+                          operateWhiteCubeAction: jest.fn(
+                              cbConfs.autoOperator?.operateWhiteCubeAction
+                          ),
+                      }
+                    : undefined,
+            },
+        }
+
+        render(<CubefulGameBoard {...next} />)
+        act(() => {
+            jest.advanceTimersByTime(10)
+        })
+        expect(next.cbConfs?.autoOperator?.operateWhiteCubeAction).toBeCalled()
+        expect(onSkipCubeAction).toBeCalled()
+        expect(state.cbState.tag).toEqual('CBToRoll')
+        expect(state.sgState.tag).toEqual('SGToRoll')
+        expect(isWhite(state.sgState)).toBeTruthy()
     })
 })
 
 afterEach(() => {
+    // clean up fake timer
     jest.runOnlyPendingTimers()
     jest.useRealTimers()
+
+    // clean up DOM
     if (container) {
         unmountComponentAtNode(container)
         container.remove()
         container = null
     }
 })
+
+function setupListeners(state: {
+    cpState?: CheckerPlayState
+    sgState: SGState
+    cbState: CBState
+}): ComponentProps<typeof CubefulGameBoard> {
+    const cpListeners: CheckerPlayListeners = setCPStateListener(
+        (next: CheckerPlayState | undefined) => {
+            state.cpState = next
+        }
+    )
+    const sgListeners: SingleGameListeners = setSGStateListener(
+        (next: SGState) => {
+            state.sgState = next
+        }
+    )
+    const cbListeners: CubeGameListeners = setCBStateListener(
+        (next: CBState) => {
+            state.cbState = next
+        }
+    )
+
+    const diceSource = presetDiceSource(1, 3, 4, 5)
+    return {
+        ...state,
+        ...cbListeners,
+        ...sgListeners,
+        ...cpListeners,
+        cbConfs: {
+            sgConfs: {
+                diceSource,
+            },
+        },
+    }
+}
+
+function setRedAutoOp(
+    props: ComponentProps<typeof CubefulGameBoard>
+): CubefulGameConfs {
+    return setAutoOp(props, redSGAutoOperator(), redCBAutoOperator())
+}
+function setWhiteAutoOp(
+    props: ComponentProps<typeof CubefulGameBoard>
+): CubefulGameConfs {
+    return setAutoOp(props, whiteSGAutoOperator(), whiteCBAutoOperator())
+}
+
+function setAutoOp(
+    props: ComponentProps<typeof CubefulGameBoard>,
+    sgOp: SGOperator,
+    cbOp: CBOperator
+): CubefulGameConfs {
+    return {
+        ...props.cbConfs,
+        sgConfs: {
+            ...props?.cbConfs?.sgConfs,
+            autoOperator: sgOp,
+        },
+        autoOperator: cbOp,
+    }
+}
+
+function isRed(sgState: SGState): boolean {
+    if (sgState.tag === 'SGOpening') {
+        return false
+    }
+    return sgState.isRed
+}
+function isWhite(sgState: SGState): boolean {
+    if (sgState.tag === 'SGOpening') {
+        return false
+    }
+    return !sgState.isRed
+}
