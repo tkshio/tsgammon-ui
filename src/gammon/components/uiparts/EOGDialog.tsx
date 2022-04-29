02abf76c4f817f46a93f0ac05931c282371ba1cf
tkshio <tkshio>
2022-04-29 03:18:50 +0000

    マッチポイントの表示を追加

--- src/gammon/components/uiparts/EOGDialog.tsx
+++ src/gammon/components/uiparts/EOGDialog.tsx
@@ -7,12 +7,31 @@ export type EOGDialogProps = {
     stake: Score
     eogStatus: EOGStatus
     score: Score
+    matchPoint?: number
+    isCrawfordNext?: boolean
     onClick: () => void
 }
 
 export function EOGDialog(props: EOGDialogProps) {
-    const { stake, eogStatus: eog, score, onClick } = { ...props }
-    const msgs = [formatStake(stake, eog), formatScore(score)]
+    const {
+        stake,
+        eogStatus: eog,
+        score,
+        matchPoint,
+        isCrawfordNext = false,
+        onClick,
+    } = { ...props }
+    const ZERO_WIDTH_SPACE = String.fromCharCode(8203)
+
+    const msgs = [
+        matchPoint ? `${matchPoint} points match` : 'Unlimited match',
+        ZERO_WIDTH_SPACE,
+        formatStake(stake, eog),
+        formatScore(score),
+        matchPoint && isCrawfordNext
+            ? 'next game is crawford'
+            : ZERO_WIDTH_SPACE,
+    ]
 
     return <Dialog msgs={msgs} onClick={onClick} />
 }
---
+++ src/stories/gammon/EOGDialog.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+import { eog, score, scoreAsRed } from 'tsgammon-core'
+
+import {
+    EOGDialog,
+    EOGDialogProps,
+} from '../../gammon/components/uiparts/EOGDialog'
+
+export default {
+    title: 'EOGDialog',
+    component: EOGDialog,
+    parameters: {},
+} as Meta
+
+const Template: Story<EOGDialogProps> = (args) => {
+    return <EOGDialog {...args} />
+}
+
+export const unlimited = Template.bind({})
+unlimited.args = {
+    stake:scoreAsRed(1),
+    eogStatus:eog(),
+    score:score(),
+    isCrawfordNext:true // should be ignored
+}
+export const pointMatch = Template.bind({})
+pointMatch.args = {
+    stake:scoreAsRed(1),
+    eogStatus:eog(),
+    score:score(),
+    matchPoint:7,
+    isCrawfordNext:false
+}
+export const crawford = Template.bind({})
+crawford.args = {
+    stake:scoreAsRed(1),
+    eogStatus:eog(),
+    score:score(),
+    matchPoint:7,
+    isCrawfordNext:true
+}
+
