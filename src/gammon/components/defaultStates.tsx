05283b116dc30524966c1290099ade179e68420f
tkshio <tkshio>
2022-05-28 03:42:34 +0000

    デフォルト値の補完

--- src/gammon/components/defaultStates.tsx
+++ src/gammon/components/defaultStates.tsx
@@ -1,14 +1,14 @@
-import { boardState, cube, GameConf } from 'tsgammon-core';
+import { boardState, cube, GameConf, standardConf } from 'tsgammon-core';
 import { cbOpening } from 'tsgammon-core/dispatchers/CubeGameState';
 import { openingState } from 'tsgammon-core/dispatchers/SingleGameState';
 
-export function defaultBGState(gameConf: GameConf) {
+export function defaultBGState(gameConf: GameConf = standardConf) {
     return {
         cbState: cbOpening(cube(1)),
         sgState: defaultSGState(gameConf),
     };
 }
 
-export function defaultSGState(gameConf: GameConf) {
+export function defaultSGState(gameConf: GameConf = standardConf) {
     return openingState(boardState(gameConf.initialPos))
 }
