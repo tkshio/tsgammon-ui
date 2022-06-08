d5b691cb06f7027da6ecb9c134979df0839e52fe
tkshio <tkshio>
2022-06-08 04:46:19 +0000

    機能追加に先行する整理

--- src/gammon/components/SingleGameBoard.tsx
+++ src/gammon/components/SingleGameBoard.tsx
@@ -28,7 +28,6 @@ import { CheckerPlayBoard, CheckerPlayBoardProps } from './CheckerPlayBoard'
 import { PositionID } from './uiparts/PositionID'
 import { useDelayedTrigger } from './utils/useDelayedTrigger'
 
-
 export type SingleGameConfs = {
     showPositionID?: boolean
     autoRoll?: boolean
@@ -42,36 +41,10 @@ export type SingleGameBoardProps = {
     dialog?: JSX.Element
 } & Partial<SingleGameEventHandlers & CheckerPlayListeners & BoardEventHandlers>
 
-function useAutoRoll(
-    sgState: SGState,
-    autoRoll: boolean,
-    onRoll: (sgState: SGToRoll) => void
-) {
-    const doRoll = useCallback(() => {
-        if (sgState.tag === 'SGToRoll') {
-            if (autoRoll) {
-                onRoll(sgState)
-            }
-        }
-        return false
-    }, [sgState, autoRoll, onRoll])
-    useDelayedTrigger(doRoll, 10)
-}
 export function SingleGameBoard(props: SingleGameBoardProps) {
-    const doNothing = () => {
-        //
-    }
-    const {
-        sgState,
-        cpState,
-        sgConfs = {},
-        cube,
-        onCommit = doNothing,
-        onRoll = doNothing,
-        onRollOpening = doNothing,
-    } = props
+    const { sgState, sgConfs = {} } = props
     const { showPositionID = true, autoRoll = false } = sgConfs
-    useAutoRoll(sgState, autoRoll, onRoll)
+    useAutoRoll(sgState, autoRoll, props.onRoll)
 
     const positionID = showPositionID && (
         <PositionID points={sgState.boardState.points} />
@@ -79,8 +52,8 @@ export function SingleGameBoard(props: SingleGameBoardProps) {
 
     const board =
         sgState.tag === 'SGInPlay'
-            ? renderBoardInPlay(sgState)
-            : renderBoard(sgState)
+            ? renderBoardInPlay(props, sgState)
+            : renderBoard(props, sgState)
 
     return (
         <Fragment>
@@ -88,43 +61,46 @@ export function SingleGameBoard(props: SingleGameBoardProps) {
             {board}
         </Fragment>
     )
-
-    function renderBoard(sgState: SGOpening | SGToRoll | SGEoG) {
-        const { onClickDice } = decorate(props, {
-            onClickDice() {
-                if (sgState.tag === 'SGOpening') {
-                    onRollOpening(sgState)
-                } else if (sgState.tag === 'SGToRoll') {
-                    onRoll(sgState)
-                }
-            },
-        })
-        const boardProps = {
-            ...props,
-            board: sgState.absBoard,
-            ...layoutDices(sgState),
-            ...layoutCube(cube),
-            onClickDice,
-        }
-        return <Board {...boardProps} />
-    }
-
-    function renderBoardInPlay(sgState: SGInPlay) {
-        // チェッカープレイ中の操作は専用のコンポーネントに任せる
-        const cpProps: CheckerPlayBoardProps = {
-            ...props,
-            cpState: cpState ?? asCheckerPlayState(sgState),
-            diceLayout: selectDiceLayout(sgState),
-
-            // チェッカープレイが確定した時に通知を受ける
-            onCommitCheckerPlay: (cpState: CheckerPlayStateCommitted) => {
-                onCommit(sgState.withNode(cpState.boardStateNode))
-                props.onCommitCheckerPlay?.(cpState)
-            },
-        }
-        return <CheckerPlayBoard {...cpProps} />
-    }
 }
+function renderBoard(
+    props: SingleGameBoardProps,
+    sgState: SGOpening | SGToRoll | SGEoG
+) {
+    const { onClickDice } = decorate(props, {
+        onClickDice() {
+            if (sgState.tag === 'SGOpening') {
+                props.onRollOpening?.(sgState)
+            } else if (sgState.tag === 'SGToRoll') {
+                props.onRoll?.(sgState)
+            }
+        },
+    })
+    const boardProps = {
+        ...props,
+        board: sgState.absBoard,
+        ...layoutDices(sgState),
+        ...layoutCube(props.cube),
+        onClickDice,
+    }
+    return <Board {...boardProps} />
+}
+
+function renderBoardInPlay(props: SingleGameBoardProps, sgState: SGInPlay) {
+    // チェッカープレイ中の操作は専用のコンポーネントに任せる
+    const cpProps: CheckerPlayBoardProps = {
+        ...props,
+        cpState: props.cpState ?? asCheckerPlayState(sgState),
+        diceLayout: selectDiceLayout(sgState),
+
+        // チェッカープレイが確定した時に通知を受ける
+        onCommitCheckerPlay: (cpState: CheckerPlayStateCommitted) => {
+            props.onCommit?.(sgState.withNode(cpState.boardStateNode))
+            props.onCommitCheckerPlay?.(cpState)
+        },
+    }
+    return <CheckerPlayBoard {...cpProps} />
+}
+
 // InPlay時以外は、SGStateから直接表示するダイスのレイアウトも内容も決まる
 function layoutDices(sgState: SGOpening | SGToRoll | SGEoG): DiceLayout {
     switch (sgState.tag) {
@@ -141,6 +117,20 @@ function layoutDices(sgState: SGOpening | SGToRoll | SGEoG): DiceLayout {
     }
 }
 
+function useAutoRoll(
+    sgState: SGState,
+    autoRoll: boolean,
+    onRoll?: (sgState: SGToRoll) => void
+) {
+    const doRoll = useCallback(() => {
+        if (sgState.tag === 'SGToRoll' && autoRoll) {
+            onRoll?.(sgState)
+        }
+        return false
+    }, [sgState, autoRoll, onRoll])
+    useDelayedTrigger(doRoll, 10)
+}
+
 // InPlay時は、ダイスはチェッカープレイに応じて変わるので、
 // 関数だけ返して動的にレイアウトする
 function selectDiceLayout(sgState: SGToRoll | SGEoG | SGInPlay) {
@@ -154,4 +144,3 @@ function layoutDicesAsRed(dices: Dice[] | BlankDice[]): DiceLayout {
 function layoutDicesAsWhite(dices: Dice[] | BlankDice[]): DiceLayout {
     return { redDices: { dices: [] }, whiteDices: { dices } }
 }
-
