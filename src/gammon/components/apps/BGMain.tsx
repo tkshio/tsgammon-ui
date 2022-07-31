089ce4f197886f733751e428ada2bf60b3c5ebd7
tkshio <tkshio>
2022-07-31 03:31:09 +0000

    ヴァリアントを追加

--- src/gammon/components/apps/BGMain.tsx
+++ src/gammon/components/apps/BGMain.tsx
@@ -70,6 +70,29 @@ const gameConfSet = {
         name: honsugorokuConf.name,
         forceCubeless: true,
     },
+    nackGammon: {
+        conf: {
+            ...standardConf,
+            name: 'Nackgammon',
+            initialPos: [
+                0, 2, 2, 0, 0, 0, -4, 0, -3, 0, 0, 0, 4, -4, 0, 0, 0, 3, 0, 4,
+                0, 0, 0, -2, -2, 0,
+            ],
+        },
+        name: 'Nackgammon',
+        forceCubeless: false,
+    },    hyperGammon: {
+        conf: {
+            ...standardConf,
+            name: 'Hypergammon',
+            initialPos: [
+                0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, -1, -1, -1, 0,
+            ],
+        },
+        name: 'Hypergammon',
+        forceCubeless: false,
+    },
 }
 type RuleLabel = keyof typeof gameConfSet
 type BGMainState = BGMainConfState | BGMainPlayState
