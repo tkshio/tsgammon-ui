980ce049e880af389b856ea0fdb4f7413ee5d038
tkshio <tkshio>
2022-07-31 03:09:59 +0000

    ダイアログ表示時に、バー上の駒が見えないのを緩和

--- src/stories/gammon/Dialog.stories.tsx
+++ src/stories/gammon/Dialog.stories.tsx
@@ -1,5 +1,4 @@
 import { Meta, Story } from '@storybook/react'
-import React from 'react'
 import { initAbsoluteBoard } from 'tsgammon-core/AbsoluteBoardState'
 import { Board } from '../../gammon/components/boards/Board'
 import { Dialog, DialogProps } from '../../gammon/components/uiparts/Dialog'
@@ -10,7 +9,6 @@ export default {
     component: Dialog,
     parameters: {},
 } as Meta
-
 const board = {
     status: '',
     board: initAbsoluteBoard(),
@@ -19,20 +17,48 @@ const board = {
     whiteScore: 0,
     redScore: 0,
 }
-const Template: Story<DialogProps> = (args) => {
-    const dialog = <Dialog {...args} />
-    return (
-        <>
-            <Board {...{ ...board, dialog }} />
-        </>
-    )
-}
+const Template: Story<DialogProps> = (args) => <Dialog {...args} />
 
 export const dialogWithBoard = Template.bind({})
 dialogWithBoard.args = {
     msgs: ['Dialog message'],
 }
+dialogWithBoard.decorators = [
+    (Story) => {
+        const dialog = <Story />
+
+        return (
+            <>
+                <Board {...{ ...board, dialog: dialog }} />
+            </>
+        )
+    },
+]
+
 export const dialogMultipleLines = Template.bind({})
 dialogMultipleLines.args = {
     msgs: ['Dialog message', 'next line'],
 }
+dialogMultipleLines.decorators = dialogWithBoard.decorators
+
+export const dialogAndOnTheBar = Template.bind({})
+dialogAndOnTheBar.args = {
+    msgs: ['Dialog message', 'next line'],
+}
+dialogAndOnTheBar.decorators = [
+    (Story) => {
+        const dialog = <Story />
+        // prettier-ignore
+        const boardOnTheBar = {...board, board:initAbsoluteBoard([
+        1,
+        0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0, 0,
+        -3        
+    ])}
+        return (
+            <>
+                <Board {...{ ...boardOnTheBar, dialog: dialog }} />
+            </>
+        )
+    },
+]
